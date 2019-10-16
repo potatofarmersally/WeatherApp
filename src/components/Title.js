ca6c1422ea5a355f426916fcde1d8b7807cda7d3
Sally <sally.ho12@yahoo.com>
2019-10-16 15:54:46 -0700

    Creating Title Component
    Got rid of default react web browser and made title component.

---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Title extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Welcome to Sally's Weather Application
+                </h1>
+                <p>
+                    Please Enter a Zipcode to See the Current Weather.
+                </p>
+            </div>
+        );
+    }
+}
